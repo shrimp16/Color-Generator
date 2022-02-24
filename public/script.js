a4ae5cb65b5ea2d86ec0f8366dd223c99a82d809
Shrimp <lcamara0916@gmail.com>
2022-02-24 11:19:23 -0100

    Added buttons to select between random colour and rainbow

--- public/script.js
+++ public/script.js
@@ -2,9 +2,19 @@ import randomColour from '../src/colours.js'
 
 const body = document.getElementById("body");
 
-document.getElementById("button").onclick = function(){
+$('#button').click(() => {
     let newColour = new randomColour();
     let newColourVal = newColour.getColour();
     console.log(newColourVal);
     body.style.backgroundColor = `rgb(${newColourVal[0]}, ${newColourVal[1]}, ${newColourVal[2]})`
-}
+})
+
+$('#random').click(() => {
+    document.querySelector('#random').style.border = "2px black solid";
+    document.querySelector('#rainbow').style.border = "";
+})
+
+$('#rainbow').click(() => {
+    document.querySelector('#rainbow').style.border = "2px black solid";
+    document.querySelector('#random').style.border = "";
+})
